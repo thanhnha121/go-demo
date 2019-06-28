00b94ac87d562eea28952ca342251a9ec5a9b66c
Nha Hoang <nhaht94@gmail.com>
2019-06-28 09:02:29 +0700

    nhaht first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-demo
+
+go 1.12
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("main")
+	color.Red("We have red")
+	color.Magenta("And many others ..")
+}
